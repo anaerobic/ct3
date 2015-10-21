b81bad2033269c799b0731b40a922b943fbba15d
Ilya Sterin <isterin@gmail.com>
2015-10-20 23:27:17 -0600

    Fixes

--- scala/app/controllers/Application.scala
+++ scala/app/controllers/Application.scala
@@ -4,6 +4,7 @@ import javax.inject.Inject
 
 import com.ltfme.loadbalancer.util.Config
 import play.api._
+import play.api.http.{HeaderNames, HttpProtocol}
 import play.api.libs.iteratee.Enumerator
 import play.api.libs.ws.{WSClient, WSResponseHeaders, WS}
 import play.api.mvc._
@@ -21,7 +22,7 @@ class Application @Inject()(ws: WSClient) extends Controller {
       val server = Config.servers(Random.nextInt(Config.servers.size))
       val headers = (request.headers.toMap.toSeq.flatMap {
         case (k, v) => v.map((k, _))
-      } filterNot { case (k,_) => k == "Host" }) :+ ("Host" -> server.host.replaceFirst("""^https?://""", ""))
+      } filterNot { case (k, _) => k == "Host" }) :+ ("Host" -> server.host.replaceFirst( """^https?://""", ""))
       val queryString = request.queryString.toSeq.flatMap {
         case (k, v) => v.map((k, _))
       }
@@ -30,23 +31,24 @@ class Application @Inject()(ws: WSClient) extends Controller {
         ws.url(server.host + request.path)
             .withFollowRedirects(true)
             .withMethod(request.method)
-            .withHeaders(headers:_*)
+            .withHeaders(headers: _*)
             .withQueryString(queryString: _*)
             .withBody(request.body.asBytes().get)
 
       println(s"REQ HEADERS: ${headers}")
       // Stream the response to the client:
-      val r = proxyRequest.get().map {
+      proxyRequest.get().map {
         r =>
-          println(s"BODY: ${r.body}")
-          Result(ResponseHeader(r.status, r.allHeaders.mapValues(_.head)), Enumerator(r.bodyAsBytes))
-      }
-      r
-    //      proxyRequest.stream.map {
-    //        case (headers: WSResponseHeaders, enum) => {
-    //          Result(ResponseHeader(headers.status, headers.headers.mapValues(_.head)), enum)
-    //        }
-    //      }
+          println(s"RES HEADERS ${r.allHeaders}")
+          println(s"BODY: ${r.bodyAsBytes}")
 
+          val resHeaders = r.allHeaders.mapValues(_.mkString(", "))
+          if (r.header(HeaderNames.TRANSFER_ENCODING).contains(HttpProtocol.CHUNKED) && r.status == OK) {
+            Ok.chunked(Enumerator(r.bodyAsBytes)).withHeaders(resHeaders.toSeq: _*)
+          }
+          else {
+            Result(ResponseHeader(r.status, resHeaders), Enumerator(r.bodyAsBytes))
+          }
+      }
   }
 }
